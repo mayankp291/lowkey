9bdbd10c25981187b21b8784f219fe1701f37c74
Mayank Panjiyara <69447074+mayankp291@users.noreply.github.com>
2021-06-27 18:12:12 +0300

    Update Vault.vue
    user list added

--- src/views/Vault.vue
+++ src/views/Vault.vue
@@ -3,7 +3,10 @@
     style="height: 180px; width: 360px;"
     class="border bg-white shadow-lg rounded-2xl m-auto mt-2 overflow-y-scroll"
   >
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="onSubmit">
+    <form
+      class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      @submit.prevent="onSubmit"
+    >
       <div class="mb-4">
         <label class="block text-gray-700 text-sm font-bold mb-2" for="website">
           Name
@@ -71,27 +74,57 @@
   </div>
   <div
     style="height: 300px; width: 360px;"
-    class="border bg-white shadow-lg rounded-2xl m-auto mt-2"
+    class="border bg-white shadow-lg rounded-2xl m-auto mt-2 overflow-y-scroll"
   >
-  
+    <table class="text-left w-full border-collapse">
+      <!--Border collapse doesn't work on this site yet but it's available in newer tailwind versions -->
+      <tbody>
+        <tr
+          class="hover:bg-grey-lighter"
+          v-for="{ id, name, username } in users"
+          :key="id"
+        >
+          <td class="py-4 px-3 border-b border-grey-light">{{ name }}</td>
+          <td class="py-4 px-3 border-b border-grey-light">{{ username }}</td>
+          <td class="py-4 px-3 border-b border-grey-light">
+            <button
+             
+              class="text-grey-lighter font-bold py-1 px-10 rounded text-xs bg-green hover:bg-green-dark"
+              >Edit</button
+            >
+            <button
+              @click="deleteUser(id)"
+              class="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark"
+              >Delete</button 
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import { createUser } from "../../firebase";
+import { createUser, useLoadUsers, deleteUser } from "../../firebase";
 import { reactive } from "vue";
 export default {
   setup() {
-    const form = reactive({ name:"" ,website: "", username: "", password: "" });
+    const users= useLoadUsers()
+    const form = reactive({
+      name: "",
+      website: "",
+      username: "",
+      password: "",
+    });
 
     const onSubmit = async () => {
       await createUser({ ...form });
-      form.name="";
+      form.name = "";
       form.website = "";
       form.username = "";
       form.password = "";
     };
-    return { form, onSubmit };
+    return { form, onSubmit, users, deleteUser };
   },
 };
 </script>
